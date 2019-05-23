b18fe8a663ee98fd9535470cb54631125fd4d872
Edgar F <eflores.ef@gmail.com>
2019-05-22 20:36:00 -0700

    get and post methods

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 const express = require('express');
 
-const server = express();
+const db = require('./data/accounts-model');
 
-// your code here
+const server = express();
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+   try {
+      const accounts = await db.find(req.query);
+      res.status(200).json(accounts);
+   } catch {
+      res.status(500).json({error: "There was an error getting the data"})
+   }
+})
+
+server.get('/:id', async (req, res) => {
+   const { id } = req.params;
+   try {
+      const account = await db.findById(id);
+      res.status(200).json(account);
+   } catch {
+      res.status(500).json({error: "There was an error getting the data"})
+   }
+})
+
+server.post('/', async (req, res) => {
+   const { body } = req;
+   try {
+      const account = await db.add(body);
+      res.status(201).json(account);
+   } catch {
+      res.status(500).json({error: "There was an error posting the data"})
+   }
+})
+
+server.put('/', (req, res) => {
+
+})
+
+server.delete('/', (req, res) => {
+   
+})
 
 module.exports = server;
